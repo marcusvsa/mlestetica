605c495e6af260d8a2f63f8e17fb48e2c4608c87
Marcus <marcus.msv1@gmail.com>
2018-12-07 00:29:58 -0200

    Commit inicial dos arquivos do Projeto.

---
+++ src/main/java/com/back/malu/api/MaluEsteticaApplication.java
@@ -0,0 +1,12 @@
+package com.back.malu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaluEsteticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaluEsteticaApplication.class, args);
+	}
+}
